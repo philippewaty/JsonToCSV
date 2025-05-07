480cb7a108117534ce867a14866ca943c6966796
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2025-05-07 10:33:36 +0200

    Code cleaning

--- JSonToCSV/Program.cs
+++ JSonToCSV/Program.cs
@@ -1,12 +1,5 @@
 ﻿using System;
 using System.Windows.Forms;
-using ChoETL;
-using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using ExceptionReporting;
 
